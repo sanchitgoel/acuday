784d0a052cb224c0fee5c3ab823922ada61ad74d
sanchitgoel <goel.sanchit@gmail.com>
2018-05-20 15:18:06 +0530

    problem added

---
+++ include/problems.php
@@ -0,0 +1,240 @@
+<section class="content">
+  <div class="container-fluid row">
+
+
+<div class="card">
+    <div class="header">
+        <h2>
+            Problems and there solutions
+            <small>Extend the default collapse behavior to create an accordion with the panel component.</small>
+        </h2>
+
+    </div>
+    <div class="body">
+      <div class="panel-group" id="accordion_4" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="headingOne_4">
+                            <h4 class="panel-title">
+                                <a role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapseOne_4" aria-expanded="true" aria-controls="collapseOne_4">
+                                    Headache
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne_4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne_4">
+                            <div class="panel-body">
+                                <div class="thumbnail"><img src="images/points/headache.jpg"></div>
+                                <div class="caption">Your LI4 acupressure point is located on the top side of the hand, between your thumb and your index finger. To locate it, squeeze the thumb against the base of the index finger. The point you are looking for is located on the highest point of the bulge of the muscle. Press this point for about 30 seconds to induce calmness and for a health-inducing digestive detox. However, you should NOT try this technique if you are pregnant.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="headingTwo_4">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapseTwo_4" aria-expanded="false"
+                                   aria-controls="collapseTwo_4">
+                                    anger and depression
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseTwo_4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo_4">
+                            <div class="panel-body">
+                                <div class="thumbnail"><img src="images/points/footPoint.jpg"></div>
+                                <div class="caption">One of the most important pressure points is found on the foot, on the line running between the big toe and the second toe and about three finger widths from the edge in the hollow on the top of the foot. Move your index finger anticlockwise over this point in order to induce relaxation and to unblock any anger and depression.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="headingThree_4">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapseThree_4" aria-expanded="false"
+                                   aria-controls="collapseThree_4">
+                                    Clear your kidneys
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseThree_4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree_4">
+                            <div class="panel-body">
+                                Your kidney point, which can be used to treat fatigue and lethargy, as well as to detox the body through the kidneys, is located on the sole of the foot, just to the side of the ball of the foot under the second or third toe. Press and release several times.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_4">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_4" aria-expanded="false"
+                                   aria-controls="collapse_4">
+                                    Pericardium
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_4">
+                            <div class="panel-body">
+                                The Pericardium (P6) acupressure point is considered useful for getting rid of nausea and vomiting caused by a variety of reasons including motion sickness, pregnancy, post-surgery and post-chemotherapy. It also relieves upset stomach, headaches, chest pain, and carpal tunnel syndrome pain and discomfort. It is located between the two large tendons on the inside of your wrist, about three finger-widths below the base of your palm. Press the point with your index and middle fingers for a couple of minutes.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_5">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_5" aria-expanded="false"
+                                   aria-controls="collapse_5">
+                                    Third Eye
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_5" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_5">
+                            <div class="panel-body">
+                                The Third Eye acupressure point is good for calming your mind, improving memory, relieving stress, chronic fatigue, headaches, eye strain and insomnia. It also helps relieve sinus pain and congestion. This point is considered beneficial for spiritual and emotional imbalances as well. Close your eyes and locate the Third Eye pressure point on the crooked spot on the bridge of your nose between your eyebrows. Using your middle finger, gently press it for a few seconds to 1 minute and then release.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_6">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_6" aria-expanded="false"
+                                   aria-controls="collapse_6">
+                                    Emotional Healing
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_6">
+                            <div class="panel-body">
+                                This acupressure point helps restore a sense of calm and relieves anxiety, nervousness, depression, hysteria and other emotional imbalances. It also helps boost the immune system. It can be easily located on the center of the breastbone, about four finger-widths up from the base of the bone. You can press this point when you are in the prayer position with your palms joined, fingers pointing upward, and the knuckles of the thumb pressing into the breastbone. Make sure your spine is straight
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_7">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_7" aria-expanded="false"
+                                   aria-controls="collapse_7">
+                                    Stomach Problems
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_7" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_7">
+                            <div class="panel-body">
+                                The ST 36 acupressure point is commonly used to improve digestive disorders including indigestion, diarrhea, constipation, bloating, gas, abdominal pain, nausea and vomiting. Plus, it boosts the immune system, fights fatigue, strengthens the body and promotes general wellness. Bend your leg and place your fingers just below the knee cap, beginning with the index finger at the base of the knee cap. You will find the point at four finger-widths down from the base of the knee cap.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_8">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_8" aria-expanded="false"
+                                   aria-controls="collapse_8">
+                                    Arthritis and Sciatica
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_8" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_8">
+                            <div class="panel-body">
+                                This knee back acupressure point (B 54) is beneficial for reducing knee and back pain; stiffness in the back; arthritis in the knee, back and hips; and sciatica pain. This acupressure point is situated at the center of the backside of the knee cap. Gently press it for about 1 minute and then repeat on the other leg. Do this daily, as needed.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_9">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_9" aria-expanded="false"
+                                   aria-controls="collapseThree_4">
+                                    Menstrual cramps
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_9" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_9">
+                            <div class="panel-body">
+                                Sacral acupressure points are great for relaxing the uterus and relieving menstrual cramps. It also helps reduce sciatica and lower back pain. The bunch of sacral points is located on the sacrum, that is, the base of the spine directly above the tail bone. Simply lie down on your back with your hands under the base of your spine. Place one hand on top of the other so as to apply steady and firm pressure on the sacral points for a couple of minutes.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_10">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_10" aria-expanded="false"
+                                   aria-controls="collapse_10">
+                                    Insomnia and Stress
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_10" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_10">
+                            <div class="panel-body">
+                                B 10 is a pair of acupressure points that are excellent for reducing stress, anxiety, exhaustion, burnout, headache or heaviness in the head, neck pain and insomnia. They are also good for relieving eye strain and a stiff neck. These points are located at one finger-width below the base of the skull on each side, on the prominent neck muscles located 1/2 inch out from the spine. Just press these acupressure points for a couple of minutes daily for several weeks.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_11">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_11" aria-expanded="false"
+                                   aria-controls="collapse_11">
+                                    Improving Concentration
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_11" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_11">
+                            <div class="panel-body">
+                                LV 3 acupressure point is good for improving memory and concentration. It encourages clarity of thought and focus. Also, it helps relieve headaches, soothe tired eyes, fight fatigue, reduce hangovers, boost the immune system and prevent allergies as it keeps the body’s energy flowing smoothly. You can easily locate this point on the top of the foot, where the bones of the big toe and second toe meet.
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_12">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_12" aria-expanded="false"
+                                   aria-controls="collapse_12">
+                                    To boost metabolism
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_12" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_12">
+                            <div class="panel-body">
+                                A metabolism-boosting point is found on top of the foot, roughly in line with the middle two toes and directly over die arch. Use two fingers to apply broader pressure here because of the bones and ligaments.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_13">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_13" aria-expanded="false"
+                                   aria-controls="collapse_13">
+                                    For immediate immunity
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_13" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_13">
+                            <div class="panel-body">
+                                An immunity-boosting point that is also good for combating fatigue, depression and general feelings of sluggishness is found on the inside of the ankle above the foot between the Achilles tendon and the ankle bone. Press and release several times for 10-20 seconds.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-danger">
+                        <div class="panel-heading" role="tab" id="heading_14">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_4" href="#collapse_14" aria-expanded="false"
+                                   aria-controls="collapse_14">
+                                    Abdomen Point
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse_14" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_14">
+                            <div class="panel-body">
+                                This pressure point is found exactly 3 centimeters below the belly button. This point enhances the function of the digestive system and gives your body strength. It also reduces constipation. This point is very effective to lose weight. Place two of your fingers on it and massage by moving your fingers up and down. Then press this point for at least 2 minutes twice a day.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </div>
+</div>
+</div>
+</section>
